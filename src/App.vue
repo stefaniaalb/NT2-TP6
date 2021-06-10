5412c414a6b468532f48d924d4e2f5c7dca3ffdf
stefaniaalb <stefania.alb95@gmail.com>
2021-06-09 23:45:45 -0300

    agrego TP6

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container-fluid mt-3">
+    <div class="jumbotron">
+      <h2>NT2: Trabajo Práctico 6</h2>
+      <br>
+      <Navbar />
+      <router-view></router-view>      
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+h1 {
+  color: blue;
+}
+
+.jumbotron {
+  padding: 32px !important;
 }
 </style>
